b85b8dafac6ffd9647135650ea7b4a99d74644a0
KaustubhDingari <speedmasterkd@gmail.com>
2022-08-26 13:05:41 -0400

    py loops

---
+++ main.py
@@ -0,0 +1,18 @@
+
+print("Hi!!!")  # print writes words and sentences you input.
+
+start= 10 # stores information TEMPORARILY.
+
+name= input("what is your name ")
+
+start= start+2 # adding to variables. start will be read as old var
+
+def type_name(name: str="kaustubh"):
+    print("help "+name )
+type_name(name=name)
+
+
+
+for i in range(2):
+    type_name(name=name)
+
